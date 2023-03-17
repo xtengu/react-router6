5fd88894bb9aa5672acdeb478366cbe9979e2602
mainzonder <no7au53r@gmail.com>
2023-03-17 13:24:44 +0200

    03 Компонент NavLink и его кастомизация в React-Router 6 |  04 Параметры в ссылках в React Router 6

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import { Homepage } from "./pages/Homepage";
 import { About } from "./pages/Aboutpage";
 import { Blogpage } from "./pages/Blogpage";
+import { Createpost } from "./pages/Createpost";
+import { Editpost } from "./pages/Editpost";
+import { Singlepage } from "./pages/Singlepage";
 import { Notfoundpage } from "./pages/Notfoundpage";
 import { Layout } from "./components/Layout";
 
@@ -13,6 +16,9 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Homepage />} />
           <Route path="/posts" element={<Blogpage />} />
+          <Route path="posts/:id" element={<Singlepage />} />
+          <Route path="posts/:id/edit" element={<Editpost />} />
+          <Route path="posts/new" element={<Createpost />} />
           <Route path="/about" element={<About />} />
           <Route path="*" element={<Notfoundpage />} />
         </Route>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,13 @@
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { CustomLink } from "./CustomLink";
 
 const Layout = () => {
   return (
     <>
       <header>
-        <Link to="/">Home</Link>
-        <Link to="/posts">Blog</Link>
-        <Link to="/about">About</Link>
+        <CustomLink to="/">Home</CustomLink>
+        <CustomLink to="/posts">Blog</CustomLink>
+        <CustomLink to="/about">About</CustomLink>
       </header>
 
       <main className="container">
