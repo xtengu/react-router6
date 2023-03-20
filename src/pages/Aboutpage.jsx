586a7e8ffc2b9026d31f46c91f614ba605009488
mainzonder <no7au53r@gmail.com>
2023-03-20 05:37:16 +0200

    07 Вложенный роутинг в React Router 6

--- src/pages/Aboutpage.jsx
+++ src/pages/Aboutpage.jsx
@@ -1,8 +1,25 @@
+import { Link, Outlet } from "react-router-dom";
+
 const About = () => {
   return (
     <div>
       <h1>About us</h1>
       <p>This is demo website about react-router-dom library</p>
+
+      <ul>
+        <li>
+          <Link to="contacts">Our Contacts</Link>
+        </li>
+        <li>
+          <Link to="team">Our Team</Link>
+        </li>
+      </ul>
+      {/* <Routes>
+        <Route path="contacts" element={<p>Our contact</p>} />
+        <Route path="team" element={<p>Our team</p>} />
+      </Routes> */}
+
+      <Outlet />
     </div>
   );
 };
